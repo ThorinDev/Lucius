b630af6ecd9d1b1a14ca320934dcebab4fefd4f7
ThorinDev <c.nethercott@icloud.com>
2016-08-08 14:28:26 +0100

    Added basic hello command

---
+++ src/com/thorindev/commands/HelloCommand.java
@@ -0,0 +1,12 @@
+package com.thorindev.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class HelloCommand implements CommandExecutor {
+
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		return false;
+	}
+}
