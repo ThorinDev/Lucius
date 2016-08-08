26d719126a61fce9da9facadf0f78cd70a053561
ThorinDev <c.nethercott@icloud.com>
2016-08-08 14:19:23 +0100

    Added onEnable and onDisable

--- src/com/thorindev/Lucius.java
+++ src/com/thorindev/Lucius.java
@@ -4,5 +4,10 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Lucius extends JavaPlugin {
 
-	
+	@Override
+	public void onEnable() {
+		getLogger().info("Lucius Made By ThorinDev");
+	}
+	@Override
+	public void onDisable() {}
 }
