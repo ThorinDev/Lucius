f0ac0cf06bdb70dea953e8dec82d9e7f8de0b5dd
ThorinDev <c.nethercott@icloud.com>
2016-08-08 14:39:34 +0100

    Having a random thing every time you do /hello

--- src/com/thorindev/commands/HelloCommand.java
+++ src/com/thorindev/commands/HelloCommand.java
@@ -1,13 +1,37 @@
 package com.thorindev.commands;
 
+import java.util.Random;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import net.md_5.bungee.api.ChatColor;
 
 public class HelloCommand implements CommandExecutor {
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if(!(sender instanceof Player)) {
+			return false;
+		}
+		else {
+			Player player = (Player) sender;
+			Random random = new Random();
+			
+			int RandomNumber = random.nextInt(5)+1;
+			
+			switch(RandomNumber) {
+				case 1:
+					player.sendMessage(ChatColor.GREEN + "Why hello there, " player.getDisplayName());
+					break;
+				case 2:
+					
+					
+			}
+			player.sendMessage(ChatColor.GREEN + "W");
+		}
 		return false;
 	}
 }
