bc82970d7e9604ab6855535346803b82791c01e9
ThorinDev <c.nethercott@icloud.com>
2016-08-08 14:29:08 +0100

    Added register Permissions method

--- src/com/thorindev/Lucius.java
+++ src/com/thorindev/Lucius.java
@@ -9,10 +9,16 @@ public class Lucius extends JavaPlugin {
 	@Override
 	public void onEnable() {
 		getLogger().info("Lucius Made By ThorinDev");
+		
+		registerPermissions();
 		registerCommands();
 	}
 	@Override
 	public void onDisable() {}
+	
+	private void registerPermissions() {
+		
+	}
 	private void registerCommands() {
 		getCommand("hello").setExecutor(new HelloCommand());
 	}
