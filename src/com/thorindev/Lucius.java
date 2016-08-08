f133fda8d7f7e8fd486fe0cb1ab06a6153464088
ThorinDev <c.nethercott@icloud.com>
2016-08-08 17:07:43 +0100

    Added some basic permisisons still need to register them though

--- src/com/thorindev/Lucius.java
+++ src/com/thorindev/Lucius.java
@@ -1,9 +1,10 @@
 package com.thorindev;
 
+import org.bukkit.permissions.Permission;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import com.thorindev.commands.HelloCommand;
 import com.thorindev.commands.FakesayCommand;
+import com.thorindev.commands.HelloCommand;
 
 public class Lucius extends JavaPlugin {
 	
@@ -25,5 +26,14 @@ public class Lucius extends JavaPlugin {
 		getCommand("hello").setExecutor(new HelloCommand()); 
 		getCommand("fakesay").setExecutor(new FakesayCommand());
 	}
+	
+	public Permission luciusMember = new Permission("lucius.member");
+	public Permission luciusAdmin = new Permission("lucius.admin");
+	
+	public Permission luciusCommandHello = new Permission("lucius.hello");
+	public Permission luciusCommandFakesay = new Permission("lucius.fakesay");
+	public Permission luciusCommandFakesayOverride = new Permission("lucius.fakesay.override");
+	public Permission luciusCommandFakesayExcempt = new Permission("lucius.fakesay.excempt");
+	
 	
 }
