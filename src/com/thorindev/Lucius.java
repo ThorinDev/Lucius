f085853e6ea5ad9289b7de4c84366a7f3794d377
ThorinDev <c.nethercott@icloud.com>
2016-08-08 14:29:00 +0100

    added register Commands method

--- src/com/thorindev/Lucius.java
+++ src/com/thorindev/Lucius.java
@@ -9,7 +9,12 @@ public class Lucius extends JavaPlugin {
 	@Override
 	public void onEnable() {
 		getLogger().info("Lucius Made By ThorinDev");
+		registerCommands();
 	}
 	@Override
 	public void onDisable() {}
+	private void registerCommands() {
+		getCommand("hello").setExecutor(new HelloCommand());
+	}
+	
 }
