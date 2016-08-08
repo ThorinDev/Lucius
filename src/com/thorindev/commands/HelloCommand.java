bcc4edf3e26db0872c037961e6ac5f3e1881d4bc
ThorinDev <c.nethercott@icloud.com>
2016-08-08 18:11:42 +0100

    Something changed here

--- src/com/thorindev/commands/HelloCommand.java
+++ src/com/thorindev/commands/HelloCommand.java
@@ -10,7 +10,7 @@ import org.bukkit.entity.Player;
 import net.md_5.bungee.api.ChatColor;
 
 public class HelloCommand implements CommandExecutor {
-	
+
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if(!(sender instanceof Player)) {
