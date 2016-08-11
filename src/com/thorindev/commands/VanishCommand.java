1ae6f2d1cf6006573baf3c57096810b527491b97
ThorinDev <c.nethercott@icloud.com>
2016-08-11 16:34:57 +0100

    Added the basic command template that I like to use it

--- src/com/thorindev/commands/VanishCommand.java
+++ src/com/thorindev/commands/VanishCommand.java
@@ -3,12 +3,21 @@ package com.thorindev.commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import net.md_5.bungee.api.ChatColor;
 
 public class VanishCommand implements CommandExecutor {
 
 	@Override
-	public boolean onCommand(CommandSender arg0, Command arg1, String arg2, String[] arg3) {
-		// TODO Auto-generated method stub
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if(!(sender instanceof Player)) {
+			sender.sendMessage(ChatColor.RED + "You need to be a player to use the command, " + ChatColor.GREEN + "/" + cmd.getName().toString());
+			return true;
+		}
+		else {
+			
+		}
 		return false;
 	}
 
