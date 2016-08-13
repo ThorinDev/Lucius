f02901316588108c24dc301bb82fdfef61e4d46f
Chris Nethercott <c.nethercott@icloud.com>
2016-08-13 13:11:27 +0100

    Added the passer (I don't know) for FakesayCommand

--- src/com/thorindev/commands/FakesayCommand.java
+++ src/com/thorindev/commands/FakesayCommand.java
@@ -6,9 +6,18 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import com.thorindev.Lucius;
+
 import net.md_5.bungee.api.ChatColor;
 
 public class FakesayCommand implements CommandExecutor {
+	
+	Lucius plugin;
+	 
+	public FakesayCommand(Lucius instance) {
+		plugin = instance;
+	}
+
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
