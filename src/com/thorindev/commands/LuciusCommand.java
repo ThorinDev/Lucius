fd8fafa027c810a1b4dd0a1d2a651138df70d6e1
Chris Nethercott <c.nethercott@icloud.com>
2016-08-14 16:54:59 +0100

    Added /lucius
    this will be used for /lucius reload

---
+++ src/com/thorindev/commands/LuciusCommand.java
@@ -0,0 +1,30 @@
+package com.thorindev.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import com.thorindev.Lucius;
+
+import net.md_5.bungee.api.ChatColor;
+
+public class LuciusCommand implements CommandExecutor {
+	
+	Lucius plugin;
+	 
+	public LuciusCommand(Lucius instance) {
+		plugin = instance;
+	}
+
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if(!(sender instanceof Player)) {
+			sender.sendMessage(ChatColor.RED + "You need to be a player to use the command, " + ChatColor.GREEN + "/" + cmd.getName().toString());
+			return true;
+		}
+		return false;
+	}
+
+}
