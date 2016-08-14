4c6594cfd80673f0289a0f72002743f540c4361d
Chris Nethercott <c.nethercott@icloud.com>
2016-08-14 12:58:31 +0100

    Added command disabled

--- src/com/thorindev/commands/ColorChatCommand.java
+++ src/com/thorindev/commands/ColorChatCommand.java
@@ -28,72 +28,77 @@ public class ColorChatCommand implements CommandExecutor {
 			String NoPermissionMessage = plugin.getConfig().getString("messages.noperm").replaceAll("(&([a-f0-9]))", "\u00A7$2");
 			String CommandDisabled = plugin.getConfig().getString("messages.commanddisabled").replaceAll("(&([a-f0-9]))", "\u00A7$2");
 			Boolean isChatColorEnabled = plugin.getConfig().getBoolean("commands.colorchat");
-			if(player.hasPermission("lucius.colorchat")) {
-				if(args.length >= 1) {
-					String Message = "";
-					for (String argument : args) {
-						Message += argument;
-						Message += " ";
-						Message = ChatColor.translateAlternateColorCodes('&', Message);
-					}
-					
-					if(Message.contains("ChatColor.BOLD")) {
-						if(player.hasPermission("lucius.chatcolor.bold")) {
-							player.chat(Message);
+			if(isChatColorEnabled == true) {
+				if(player.hasPermission("lucius.colorchat")) {
+					if(args.length >= 1) {
+						String Message = "";
+						for (String argument : args) {
+							Message += argument;
+							Message += " ";
+							Message = ChatColor.translateAlternateColorCodes('&', Message);
+						}
+						
+						if(Message.contains("ChatColor.BOLD")) {
+							if(player.hasPermission("lucius.chatcolor.bold")) {
+								player.chat(Message);
+							}
+							else {
+								player.sendMessage(ChatColor.RED + "You do not have permission to use bold formatting");
+							}
+						}
+						else if(Message.contains("ChatColor.UNDERLINE")) {
+							if(player.hasPermission("lucius.chatcolor.underline")) {
+								player.chat(Message);
+							}
+							else {
+								player.sendMessage(ChatColor.RED + "You do not have permission to use underline formatting");
+							}
+						}
+						else if(Message.contains("ChatColor.ITALLIC")) {
+							if(player.hasPermission("lucius.chatcolor.itallic")) {
+								player.chat(Message);
+							}
+							else {
+								player.sendMessage(ChatColor.RED + "You do not have permission to use itallic formating");
+							}	
+						}
+						else if(Message.contains("ChatColor.MAGIC")){
+							if(player.hasPermission("lucius.chatcolor.magic")) {
+								player.chat(Message);
+							}
+							else {
+								player.sendMessage(ChatColor.RED + "You do not have permission to use magic formatting");
+							}
+						}
+						else if(Message.contains("ChatColor.STRIKETHROUGH")) {
+							if(player.hasPermission("lucius.chatcolor.strike")) {
+								player.chat(Message);
+							}
+							else {
+								player.sendMessage(ChatColor.RED + "You do not have permission to use strikethrough formatting");
+							}
 						}
 						else {
-							player.sendMessage(ChatColor.RED + "You do not have permission to use bold formatting");
-						}
-					}
-					else if(Message.contains("ChatColor.UNDERLINE")) {
-						if(player.hasPermission("lucius.chatcolor.underline")) {
 							player.chat(Message);
 						}
-						else {
-							player.sendMessage(ChatColor.RED + "You do not have permission to use underline formatting");
-						}
-					}
-					else if(Message.contains("ChatColor.ITALLIC")) {
-						if(player.hasPermission("lucius.chatcolor.itallic")) {
-							player.chat(Message);
-						}
-						else {
-							player.sendMessage(ChatColor.RED + "You do not have permission to use itallic formating");
-						}	
-					}
-					else if(Message.contains("ChatColor.MAGIC")){
-						if(player.hasPermission("lucius.chatcolor.magic")) {
-							player.chat(Message);
-						}
-						else {
-							player.sendMessage(ChatColor.RED + "You do not have permission to use magic formatting");
-						}
-					}
-					else if(Message.contains("ChatColor.STRIKETHROUGH")) {
-						if(player.hasPermission("lucius.chatcolor.strike")) {
-							player.chat(Message);
-						}
-						else {
-							player.sendMessage(ChatColor.RED + "You do not have permission to use strikethrough formatting");
-						}
+						
+						
+						return true;
 					}
 					else {
-						player.chat(Message);
+						player.sendMessage(ChatColor.RED + "You need to say what you want to say");
+						return true;
 					}
-					
-					
-					return true;
 				}
 				else {
-					player.sendMessage(ChatColor.RED + "You need to say what you want to say");
-					return true;
+					player.sendMessage(ChatColor.RED + NoPermissionMessage);
+					
 				}
-			}
-			else {
-				player.sendMessage(ChatColor.RED + NoPermissionMessage);
-				return true;
-			}
 		}
+		else {
+			player.sendMessage(CommandDisabled);
+		}
+		return false;
 	}
 
-}
+}}
