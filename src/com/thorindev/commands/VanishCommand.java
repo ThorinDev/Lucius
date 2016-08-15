b47eb44b7ea37ea9c5e71f73210c07612fea1bae
Chris Nethercott <c.nethercott@icloud.com>
2016-08-15 13:46:28 +0100

    Redoing on Vanish!

--- src/com/thorindev/commands/VanishCommand.java
+++ src/com/thorindev/commands/VanishCommand.java
@@ -36,45 +36,12 @@ public class VanishCommand implements CommandExecutor, Listener {
 		else {
 			Player player = (Player) sender;
 			if(player.hasPermission("lucius.vanish")) {
-				 if (!vanished.contains(player)) {
-                     for (Player toHide : Bukkit.getServer().getOnlinePlayers()) {
-                             toHide.hidePlayer(player);
-                     }
-                     vanished.add(player);
-                     player.sendMessage(ChatColor.GREEN + "You have been vanished");
-                     return true;
-				 }
-				 else {
-                     for (Player toShow : Bukkit.getServer().getOnlinePlayers()) {
-                             toShow.showPlayer(player);
-                     }
-                     vanished.remove(player);
-                     player.sendMessage(ChatColor.GREEN + "You have been unvanished");
-                     return true;
-				 }		 
-			}
+				return true;
+			}	
 			else {
 				player.sendMessage(ChatColor.RED + "You do not have permission to use this command");
-				return true;
-				
+				return true;	
 			}
-		}
-	}
-	
-	@EventHandler
-	public void onPlayerJoinVanish(PlayerJoinEvent event) {
-		if(vanished.contains(event.getPlayer())) {
-			event.getPlayer().hidePlayer(event.getPlayer());
-		}
-		for(Player player : vanished) {
-			event.getPlayer().hidePlayer(player);
-		}
-	}
-	
-	@EventHandler
-	public void onPlayerLeaveVanish(PlayerQuitEvent event) {
-		if(vanished.contains(event.getPlayer())) {
-			vanished.remove(event.getPlayer());
 		}
 	}
 
