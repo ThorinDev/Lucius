78e0dbad816d0e26ef9a6a383999f2ad7c710e14
Chris Nethercott <c.nethercott@icloud.com>
2016-08-16 12:19:26 +0100

    Updated a load for things. Using switch

--- src/com/thorindev/commands/FWCommand.java
+++ src/com/thorindev/commands/FWCommand.java
@@ -59,10 +59,8 @@ public class FWCommand implements CommandExecutor {
 		            if (rt == 5) type = Type.STAR;
 		           
 		            //Get our random colours   
-		            int r1i = r.nextInt(17) + 1;
-		            int r2i = r.nextInt(17) + 1;
-		            Color c1 = getColor(r1i);
-		            Color c2 = getColor(r2i);
+		            Color c1 = getColor();
+		            Color c2 = getColor();
 		           
 		            //Create our effect with this
 		            FireworkEffect effect = FireworkEffect.builder().flicker(r.nextBoolean()).withColor(c1).withFade(c2).with(type).trail(r.nextBoolean()).build();
@@ -90,63 +88,66 @@ public class FWCommand implements CommandExecutor {
 		}
 	}
 	
-	private Color getColor(int i) {
+	private Color getColor() {
 		Color c = null;
-		if(i==1){
-		c=Color.AQUA;
-		}
-		if(i==2){
-		c=Color.BLACK;
-		}
-		if(i==3){
-		c=Color.BLUE;
-		}
-		if(i==4){
-		c=Color.FUCHSIA;
-		}
-		if(i==5){
-		c=Color.GRAY;
-		}
-		if(i==6){
-		c=Color.GREEN;
-		}
-		if(i==7){
-		c=Color.LIME;
-		}
-		if(i==8){
-		c=Color.MAROON;
-		}
-		if(i==9){
-		c=Color.NAVY;
-		}
-		if(i==10){
-		c=Color.OLIVE;
-		}
-		if(i==11){
-		c=Color.ORANGE;
-		}
-		if(i==12){
-		c=Color.PURPLE;
-		}
-		if(i==13){
-		c=Color.RED;
-		}
-		if(i==14){
-		c=Color.SILVER;
-		}
-		if(i==15){
-		c=Color.TEAL;
-		}
-		if(i==16){
-		c=Color.WHITE;
-		}
-		if(i==17){
-		c=Color.YELLOW;
-		}
-		 
+		Random random = new Random();
+		int i = random.nextInt(17)+1;
+		
+		switch(i) {
+			case 1:  
+				c=Color.AQUA;
+				break;
+			case 2:  
+				c=Color.BLACK;
+				break;
+			case 3:  
+				c=Color.BLUE;
+				break;
+			case 4:  
+				c=Color.FUCHSIA;
+				break;
+			case 5:  
+				c=Color.GREEN;
+				break;
+			case 6:  
+				c=Color.LIME;
+				break;
+			case 7:  
+				c=Color.MAROON;
+				break;
+			case 8:  
+				c=Color.NAVY;
+				break;
+			case 9:  
+				c=Color.OLIVE;
+				break;
+			case 10:  
+				c=Color.ORANGE;
+				break;
+			case 11:  
+				c=Color.PURPLE;
+				break;
+			case 12:  
+				c=Color.RED;
+				break;
+			case 13:  
+				c=Color.SILVER;
+				break;
+			case 14:  
+				c=Color.TEAL;
+				break;
+			case 15:  
+				c=Color.WHITE;
+				break;
+			case 16:  
+				c=Color.YELLOW;
+				break;
+			case 17:  
+				c=Color.AQUA;
+				break;	
+		}	 
 		return c;
 		}
-
 
 
 }
