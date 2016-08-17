2ad1a5d9c2a1d7fa8459c0bed9c35ca328a7639b
Chris Nethercott <c.nethercott@icloud.com>
2016-08-17 17:02:16 +0200

    This works completely.

--- src/com/thorindev/commands/FWCommand2.java
+++ src/com/thorindev/commands/FWCommand2.java
@@ -1,10 +1,18 @@
 package com.thorindev.commands;
 
 import java.util.HashMap;
+import java.util.Random;
+
+import org.bukkit.Color;
+import org.bukkit.FireworkEffect;
+import org.bukkit.FireworkEffect.Type;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Firework;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.meta.FireworkMeta;
 
 import com.thorindev.Lucius;
 
@@ -35,17 +43,40 @@ public class FWCommand2 implements CommandExecutor {
 			Boolean isFWEnabled = plugin.getConfig().getBoolean("commands.fw");
 			if(isFWEnabled == true) {
 				if(player.hasPermission("lucius.fw")) {
-					int cooldownTime = 60; // Get number of seconds from wherever you want
-			        if(cooldowns.containsKey(player.getName())) {
-			            long secondsLeft = ((cooldowns.get(player.getName())/1000)+cooldownTime) - (System.currentTimeMillis()/1000);
-			            if(secondsLeft > 0) {
-			                player.sendMessage("You cant use that commands for another "+ secondsLeft +" seconds!");
+					int cooldownTime = 15; // Get number of seconds from wherever you want
+			        if(cooldowns.containsKey(sender.getName())) {
+			            long secondsLeft = ((cooldowns.get(sender.getName())/1000)+cooldownTime) - (System.currentTimeMillis()/1000);
+			            if(secondsLeft>0) {
+			                // Still cooling down
+			                sender.sendMessage("You cant use that commands for another "+ secondsLeft +" seconds!");
 			                return true;
 			            }
 			        }
-			        cooldowns.put(player.getName(), System.currentTimeMillis());
-			        player.sendMessage("Command Activated!");
-			        return true; 
+			        // No cooldown found or cooldown has expired, save new cooldown
+			        cooldowns.put(sender.getName(), System.currentTimeMillis());
+			        Firework fw = (Firework) player.getWorld().spawnEntity(player.getLocation(), EntityType.FIREWORK);
+					FireworkMeta fwm = fw.getFireworkMeta();
+					Random r = new Random();
+					int rt = r.nextInt(4) + 1;
+					Type type = Type.BALL;
+					if (rt == 1)
+						type = Type.BALL;
+					if (rt == 2)
+						type = Type.BALL_LARGE;
+					if (rt == 3)
+						type = Type.BURST;
+					if (rt == 4)
+						type = Type.CREEPER;
+					if (rt == 5)
+						type = Type.STAR;
+					Color c1 = getColor();
+					Color c2 = getColor();
+					FireworkEffect effect = FireworkEffect.builder().flicker(r.nextBoolean()).withColor(c1).withFade(c2).with(type).trail(r.nextBoolean()).build();
+					fwm.addEffect(effect);
+					int rp = r.nextInt(2) + 1;
+					fwm.setPower(rp);
+					fw.setFireworkMeta(fwm);
+			        return true;
 				}
 				else {
 					player.sendMessage(NPMColor);
@@ -57,5 +88,65 @@ public class FWCommand2 implements CommandExecutor {
 				return true;
 			}
 		}
+	}
+	private Color getColor() {
+		Color c = null;
+		Random random = new Random();
+		int i = random.nextInt(17)+1;
+		
+		switch(i) {
+			case 1:  
+				c = Color.AQUA;
+				break;
+			case 2:  
+				c = Color.BLACK;
+				break;
+			case 3:  
+				c = Color.BLUE;
+				break;
+			case 4:  
+				c = Color.FUCHSIA;
+				break;
+			case 5:  
+				c = Color.GREEN;
+				break;
+			case 6:  
+				c = Color.LIME;
+				break;
+			case 7:  
+				c = Color.MAROON;
+				break;
+			case 8:  
+				c = Color.NAVY;
+				break;
+			case 9:  
+				c = Color.OLIVE;
+				break;
+			case 10:  
+				c = Color.ORANGE;
+				break;
+			case 11:  
+				c = Color.PURPLE;
+				break;
+			case 12:  
+				c = Color.RED;
+				break;
+			case 13:  
+				c = Color.SILVER;
+				break;
+			case 14:  
+				c = Color.TEAL;
+				break;
+			case 15:  
+				c = Color.WHITE;
+				break;
+			case 16:  
+				c = Color.YELLOW;
+				break;
+			case 17:  
+				c = Color.AQUA;
+				break;	
+		}	 
+		return c;
 	}
 }
