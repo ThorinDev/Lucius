56d7fa90969b96102815134363a37f97f7c121b6
Chris Nethercott <c.nethercott@icloud.com>
2016-08-27 14:15:19 +0200

    [CHANGE] Using an array for firework colours
    Am doing the types next.

--- src/com/thorindev/commands/FWCommand.java
+++ src/com/thorindev/commands/FWCommand.java
@@ -107,60 +107,27 @@ public class FWCommand implements CommandExecutor {
 	}
 	private Color getColor() {
 		Color c = null;
-		int i = random.nextInt(17)+1;	
-		switch(i) {
-			case 1:  
-				c = Color.AQUA;
-				break;
-			case 2:  
-				c = Color.BLACK;
-				break;
-			case 3:  
-				c = Color.BLUE;
-				break;
-			case 4:  
-				c = Color.FUCHSIA;
-				break;
-			case 5:  
-				c = Color.GREEN;
-				break;
-			case 6:  
-				c = Color.GRAY;
-				break;	
-			case 7:  
-				c = Color.LIME;
-				break;
-			case 8:  
-				c = Color.MAROON;
-				break;
-			case 9:  
-				c = Color.NAVY;
-				break;
-			case 10:  
-				c = Color.OLIVE;
-				break;
-			case 11:  
-				c = Color.ORANGE;
-				break;
-			case 12:  
-				c = Color.PURPLE;
-				break;
-			case 13:  
-				c = Color.RED;
-				break;
-			case 14:  
-				c = Color.SILVER;
-				break;
-			case 15:  
-				c = Color.TEAL;
-				break;
-			case 16:  
-				c = Color.WHITE;
-				break;
-			case 17:  
-				c = Color.YELLOW;
-				break;
-		}	 
+		int i = random.nextInt(17)+1;
+		Color[] listOfColors = {
+			Color.AQUA,
+			Color.BLACK,
+			Color.BLUE,
+			Color.FUCHSIA,
+			Color.GREEN,
+			Color.GRAY,
+			Color.LIME,
+			Color.MAROON,
+			Color.NAVY,
+			Color.OLIVE,
+			Color.ORANGE,
+			Color.PURPLE,
+			Color.RED,
+			Color.SILVER,
+			Color.TEAL,
+			Color.WHITE,
+			Color.YELLOW
+		};
+		c = listOfColors[i];
 		return c;
 	}
 	private Type getType() {
