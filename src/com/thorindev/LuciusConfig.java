b0d9ee73958c57503c4bff1b583c10e9c8f86034
ThorinDev <c.nethercott@icloud.com>
2016-09-09 19:04:14 +0100

    Added LuciusConfi

---
+++ src/com/thorindev/LuciusConfig.java
@@ -0,0 +1,40 @@
+package com.thorindev;
+
+import org.bukkit.configuration.file.FileConfiguration;
+
+public class LuciusConfig {
+	
+	Lucius plugin;
+	 
+	public LuciusConfig(Lucius instance) {
+		plugin = instance;
+	}
+
+	public void registerConfig() {
+		FileConfiguration config = plugin.config;
+		config.options().header(
+				"Lucius \n" +
+				"All commands can be disbaled and enabled, as well as changing other things such as the firework timeout \n" +
+				"You can also change messages for command disabled and no permission, with full formatting support"
+		);
+		config.addDefault("commands.colorchat", true);
+		config.addDefault("commands.fakesay", true);
+		config.addDefault("commands.fw", false);
+		config.addDefault("commands.fwtimeout",  15);
+		config.addDefault("commands.hello", true);
+		config.addDefault("commands.vanish", true);
+		config.addDefault("messages.noperm", "&cYou do not have permission to use this command");
+		config.addDefault("messages.commanddisabled", "&cCommand Disabled");
+		config.options().copyDefaults(true);
+		config.options().copyHeader(true);
+		saveConfig();
+		
+	}
+
+	private void saveConfig() {
+		plugin.saveConfig();
+	}
+	
+    
+
+}
