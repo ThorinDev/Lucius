23b2972fb6e2fa79710652fa37245564a408d079
Chris <c.nethercott@icloud.com>
2016-10-25 20:41:41 +0100

    Renamed ChatColor to ChatColorListener due to compatibilly issues

--- src/com/thorindev/listeners/ChatColorListener.java
+++ src/com/thorindev/listeners/ChatColorListener.java
@@ -5,11 +5,20 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
-public class ChatColor implements Listener {
+import net.md_5.bungee.api.ChatColor;
+
+public class ChatColorListener implements Listener {
 	
 	@EventHandler
 	public void onPlayerChatEvent(AsyncPlayerChatEvent e) {
 		Player p = e.getPlayer();
+		String m = e.getMessage();
+		if(p.hasPermission("lucius.chatcolor")) {
+			if(m.contains("&")) {
+				e.setCancelled(true);
+				p.chat(ChatColor.translateAlternateColorCodes('&', m));
+			}
+		}
 	}
 	
 }
