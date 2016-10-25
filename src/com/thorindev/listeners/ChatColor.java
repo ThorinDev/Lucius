631619679756aa16f04cd0da7453e54f5e362709
Chris <c.nethercott@icloud.com>
2016-10-25 20:34:42 +0100

    Added main things in the class

--- src/com/thorindev/listeners/ChatColor.java
+++ src/com/thorindev/listeners/ChatColor.java
@@ -1,5 +1,15 @@
 package com.thorindev.listeners;
 
-public class ChatColor {
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 
+public class ChatColor implements Listener {
+	
+	@EventHandler
+	public void onPlayerChatEvent(AsyncPlayerChatEvent e) {
+		Player p = e.getPlayer();
+	}
+	
 }
